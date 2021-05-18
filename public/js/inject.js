eee66645bcf4f8b44324e5f99a7eaaf566b6b439
Gav Hern <contact@gavhern.com>
2021-05-17 21:10:59 -0700

    🗑️ Remove dashboard sidenav component

--- public/js/inject.js
+++ public/js/inject.js
@@ -1,35 +1,4 @@
 "use strict";
-function appendDashboardSidenavLink() {
-    if (document.querySelector('#better-slp-dashboard-link') != null)
-        return;
-    const dashboardNavigationContainer = document.createElement('li');
-    dashboardNavigationContainer.id = "better-slp-dashboard-link";
-    dashboardNavigationContainer.setAttribute('role', 'presentation');
-    const dashboardNavigationLink = dashboardNavigationContainer.appendChild(document.createElement('a'));
-    dashboardNavigationLink.setAttribute('action', 'push');
-    dashboardNavigationLink.setAttribute('href', '/my/dashboard');
-    const dashboardNavigationIcon = dashboardNavigationLink.appendChild(document.createElement('i'));
-    dashboardNavigationIcon.setAttribute('aria-hidden', 'true');
-    ['material-icons', 'material-icons-v54', 'material-icons-default', 'app-nav-left-link-icon'].forEach(className => {
-        dashboardNavigationIcon.classList.add(className);
-    });
-    dashboardNavigationIcon.innerHTML = "dashboard";
-    dashboardNavigationLink.append("Dashboard");
-    dashboardNavigationLink.addEventListener('click', async function (e) {
-        e.preventDefault();
-        const applicationBody = document.querySelector('.app-body');
-        if (applicationBody == null)
-            return;
-        window.history.pushState({}, "", "/my/dashboard");
-        document.querySelectorAll('.page-menu .app-page-menu-body > ul.nav > li.active').forEach(navElement => {
-            navElement.classList.remove('active');
-        });
-        this.parentElement?.classList.add('active');
-        let newPageContents = await fetch(chrome.runtime.getURL('html/dashboard.html'));
-        applicationBody.innerHTML = await newPageContents.text();
-    });
-    document.querySelector('.page-menu .app-page-menu-body > ul.nav')?.prepend(dashboardNavigationContainer);
-}
 function openNoteTaker() {
     const mainContentContainer = document.querySelector('.claro-student-focusarea');
     mainContentContainer?.querySelector('.panel-body')?.classList.add('hidden');
@@ -68,7 +37,6 @@ function appendNoteTaker() {
     document.querySelector('.app-body > .app-page-title > .app-page-title-row')?.appendChild(takeNotesButton);
 }
 function main() {
-    appendDashboardSidenavLink();
     const currentURL = window.location.href;
     switch (true) {
         case new RegExp('.*:\/\/.*?\.?summitlearning\.org/my/focusareas/.*').test(currentURL):
