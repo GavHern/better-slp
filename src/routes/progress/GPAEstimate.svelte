d22cb3e950525e9037444ea6cf076095cd29a8c8
Gav Hern <contact@gavhern.com>
2023-01-14 21:48:39 -0800

    Fetch data from API for gpa estimation

--- src/routes/progress/GPAEstimate.svelte
+++ src/routes/progress/GPAEstimate.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-  let unweighted = 3.94;
-  let weighted = 4.29;
+  let res = fetch("https://www.summitlearning.org/my/progress.json").then(
+    (res) => res.json()
+  );
+
+  const calculateGPA = (weighted, data) => {
+    return 4;
+  };
+
+  const formatNumber = (number) =>
+    new Intl.NumberFormat("en-US", { minimumFractionDigits: 2 }).format(number);
 </script>
 
 <div class="claro-list-group sdl-course-grades list-group">
@@ -15,33 +23,45 @@
       </div>
     </div>
   </span>
-  <li class="grade-row list-group-item">
-    <div class="row">
-      <div class="course-name-column col-xs-6">
-        <span class="course-name">Unweighted</span>
+  {#await res}
+    <div class="loading-indicator-icon large" />
+  {:then data}
+    {@const unweighted = calculateGPA(false, data)}
+    {@const weighted = calculateGPA(true, data)}
+    <li class="grade-row list-group-item">
+      <div class="row">
+        <div class="course-name-column col-xs-6">
+          <span class="course-name">Unweighted</span>
+        </div>
+        <div class="grade-column col-xs-5">
+          <span class="current-grade grade">
+            <span class="letter-grade">{formatNumber(unweighted)}</span>
+          </span>
+        </div>
       </div>
-      <div class="grade-column col-xs-5">
-        <span class="current-grade grade">
-          <span class="letter-grade">{unweighted}</span>
-        </span>
+    </li>
+    <li class="grade-row list-group-item">
+      <div class="row">
+        <div class="course-name-column col-xs-6">
+          <span class="course-name">Weighted</span>
+        </div>
+        <div class="grade-column col-xs-5">
+          <span class="current-grade grade">
+            <span class="letter-grade">{formatNumber(unweighted)}</span>
+          </span>
+        </div>
       </div>
-    </div>
-  </li>
-  <li class="grade-row list-group-item">
-    <div class="row">
-      <div class="course-name-column col-xs-6">
-        <span class="course-name">Weighted</span>
-      </div>
-      <div class="grade-column col-xs-5">
-        <span class="current-grade grade">
-          <span class="letter-grade">{weighted}</span>
-        </span>
-      </div>
-    </div>
-  </li>
+    </li>
+  {:catch}
+    There was an error calculating GPA estimates.
+  {/await}
 </div>
 
 <style>
+  .loading-indicator-icon {
+    margin-top: 1.5em;
+  }
+
   .bslp-gpa-info-tooltip {
     color: #00000070;
     padding-left: 0.3em;
