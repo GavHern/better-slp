07fcc0209a89a96a7da73afd8c08dc615ffe9bf1
Gav Hern <contact@gavhern.com>
2023-01-29 21:07:08 -0800

    Restrict extension to only have access to summit learning

--- src/worker.ts
+++ src/worker.ts
@@ -1,10 +1,11 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.url) {
+  if (changeInfo.url?.includes("summitlearning.org")) {
     chrome.tabs.get(tabId, (tab) => {
       if (tab) {
         chrome.tabs.sendMessage(tabId, {
           message: "routerUpdate",
           url: changeInfo.url,
+          status: changeInfo.status,
         });
       }
     });
