bd5ca30cff4096a0a8745e4b66144541163a0639
Gav Hern <contact@gavhern.com>
2023-02-06 23:44:33 -0800

    Add note taker introduction

--- src/routes/focusArea/NoteTaker.svelte
+++ src/routes/focusArea/NoteTaker.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+  import NoteTakerIntroduction from "./NoteTakerIntroduction.svelte";
   import { onMount } from "svelte";
   import Quill from "quill";
+
+  let showIntroduction = JSON.parse(window.localStorage.getItem("bslp-note-taker-show-intro")) ?? true;
 
   let editor;
   let quill;
@@ -10,20 +13,23 @@
   onMount(() => {
     window.onbeforeunload = () => "You have unsaved changes!";
 
+    const toolbarOptions = [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ font: [] }],
+      ["bold", "italic", "underline", "strike"],
+      [{ color: [] }, { background: [] }],
+      ["link", "image", "video", "blockquote"],
+      [{ list: "bullet" }, { list: "ordered" }],
+      [{ align: [] }],
+      [{ indent: "-1" }, { indent: "+1" }],
+      [{ script: "sub" }, { script: "super" }],
+      ["clean"],
+    ];
+
+    // @ts-ignore
     quill = new Quill(editor, {
       modules: {
-        toolbar: [
-          [{ header: [1, 2, 3, 4, 5, 6, false] }],
-          [{ font: [] }],
-          ["bold", "italic", "underline", "strike"],
-          [{ color: [] }, { background: [] }],
-          ["link", "image", "video", "blockquote"],
-          [{ list: "bullet" }, { list: "ordered" }],
-          [{ align: [] }],
-          [{ indent: "-1" }, { indent: "+1" }],
-          [{ script: "sub" }, { script: "super" }],
-          ["clean"],
-        ],
+        toolbar: toolbarOptions,
       },
       theme: "snow",
       placeholder: "Write your notes here...",
@@ -52,6 +58,11 @@
 
     window.onbeforeunload = null;
   };
+
+  const introductionExit = () => {
+    showIntroduction = false;
+    window.localStorage.setItem("bslp-note-taker-show-intro", "false");
+  };
 </script>
 
 <svelte:window
@@ -65,7 +76,15 @@
 
 <div class="bslp-route-specific bslp-notes bslp-note-taker-container" data-id={id}>
   <div class="bslp-note-taker">
+    {#if showIntroduction}
+      <NoteTakerIntroduction on:exit={introductionExit} />
+    {/if}
+
     <div class="bslp-quill-editor" bind:this={editor} />
+    <!-- <button class="bslp-note-taker-save-button">
+      <i class="material-icons material-icons-default">save</i>
+      Save without closing
+    </button> -->
   </div>
 </div>
 
@@ -87,17 +106,18 @@
     z-index: 50000
     pointer-events: none
 
-  .bslp-note-taker
-    display: flex
-    flex-direction: column
-    pointer-events: auto
-    width: min(80%, 1200px)
-    height: 75%
-    background: #ffffff
-    margin: auto
-    border-radius: 3px
-    /* box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px */
-    box-shadow: 0 5px 15px rgb(0 0 0 / 50%)
+	.bslp-note-taker
+		display: flex
+		flex-direction: column
+		position: relative
+		pointer-events: auto
+		width: min(80%, 1200px)
+		height: 75%
+		background: #ffffff
+		margin: auto
+		border-radius: 3px
+		/* box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px */
+		box-shadow: 0 5px 15px rgb(0 0 0 / 50%)
 
   .bslp-quill-editor 
     height: calc(100% - 42px)
---
+++ src/routes/focusArea/NoteTakerIntroduction.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { scale, fade } from "svelte/transition";
+
+  const dispatch = createEventDispatcher();
+
+  const getStarted = () => {
+    dispatch("exit");
+  };
+</script>
+
+<div class="bslp-note-taker-intro-container" transition:fade={{ duration: 250 }}>
+  <div class="bslp-note-taker-intro-modal" transition:scale={{ start: 0.7, duration: 250 }}>
+    <h2>BetterSLP Note Taker</h2>
+    <p>
+      The BetterSLP note taker allows you to seamlessly take notes on a focus area. Here are some important things to
+      know before you get started:
+    </p>
+    <ul>
+      <li>Notes are automatically saved when you close the note editor</li>
+      <li>Notes get saved to the current device you are using, meaning they will not be synced across devices</li>
+      <li>If you wish to discard any changes to the note you are working on, simply refresh the page</li>
+    </ul>
+
+    <button on:click={getStarted}>
+      Get started!
+      <i class="material-icons material-icons-default">arrow_forward</i>
+    </button>
+  </div>
+</div>
+
+<style lang="stylus">
+	.bslp-note-taker-intro-container
+		width: 100%
+		height: 100%
+		position: absolute
+		inset: 0
+		z-index: 50000
+		background-color: rgba(22, 27, 31, 0.5)
+
+		.bslp-note-taker-intro-modal
+			position: absolute
+			inset: 0
+			margin: auto
+			width: 24rem
+			height: 32rem
+			padding: 1rem
+			box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+			background: linear-gradient(135deg, rgba(87,81,210,1) 0%, rgba(0,212,255,1) 100%);
+			border-radius 0.5rem
+
+			h2, p, ul, button
+				color: #ffffff !important
+				margin-bottom: 1rem
+				font-size: 1.1em
+
+			h2
+				font-size: 2.2em
+				font-weight: 500
+
+			ul
+				list-style: disc
+				padding-inline-start: 24px
+
+			button
+				margin-top: 1rem
+				border: 2px solid white
+				text-transform: uppercase
+				font-weight: 500
+				padding: 0.75rem 1rem
+				border-radius: 0.5rem
+				transition all 300ms
+
+				i
+					padding-left: 4px
+
+				&:hover
+					background-color: #ffffff
+					color: rgba(87,81,210,1) !important
+</style>
